0802691eb8c0b053be9328926d0cb814a1a56462
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2012-12-10 07:58:10 +0900

    Swift3 1.7
    Added a versin tag for distribution packages.
    
    Likely Swift3 1.7 works with Swift 1.7.0 and the previous versions
    however not with the following verions (1.7.1 and so on).
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- swift3/__init__.py
+++ swift3/__init__.py
@@ -19,6 +19,6 @@ Static Web Middleware for OpenStack Swift
 __all__ = ['version_info', 'version']
 
 #: Version information ``(major, minor, revision)``.
-version_info = (1, 0, 0)
+version_info = (1, 7, 0)
 #: Version string ``'major.minor.revision'``.
 version = '.'.join(map(str, version_info))
