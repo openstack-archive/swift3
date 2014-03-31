500f40d955e0943e01b241605b9f3d16cba99f7e
Nassim Babaci <nassim.babaci@cloudwatt.com>
2014-03-31 11:11:37 +0200

    Add setup.cfg and pbr support.
    This patch fix a bug when installing a swift3 on
    devstack.
    
    Because devstack uses pbr/requirements.txt to
    sync global requirements, we need to add setup.cfg and
    pbr support to swift3 so it can be installed on devstack.
    
    This also allow to take advantage of the pbr facilities
    (version number support, etc)
    
    Change-Id: Id0179fad179efac5ba85bef7838365a4d304d196

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-# Copyright (c) 2012-2014 OpenStack Foundation.
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -9,22 +9,14 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import setuptools
 
-import swift3
-
-
-setup(name='swift3',
-      version=swift3.version,
-      description='Swift AmazonS3 API emulation Middleware',
-      author='OpenStack Foundation',
-      author_email='openstack-dev@lists.openstack.org',
-      url='https://github.com/stackforge/swift3',
-      packages=['swift3'],
-      requires=['swift(>=1.4)'],
-      entry_points={'paste.filter_factory':
-          ['swift3=swift3.middleware:filter_factory']})
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
