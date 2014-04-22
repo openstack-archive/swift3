530297a29150736208cd30c018a427f9d7e2d2eb
MORITA Kazutaka <morita.kazutaka@gmail.com>
2014-04-22 15:40:16 +0900

    Remove pbr dependency at run time
    This change is based on the following commit in the Swift tree.
    
      0717133 Make pbr a build-time only dependency
    
    Change-Id: I43956f531a9928ade296236b3b605e52dc2f86f3

--- swift3/__init__.py
+++ swift3/__init__.py
@@ -12,20 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Static Web Middleware for OpenStack Swift
-"""
-import pbr.version
+import pkg_resources
 
 __all__ = ['version_info', 'version']
 
-# get version info using pbr.version.
-# pbr version info is inferred from version in setup.cfg
-# and vcs information.
-_version_info = pbr.version.VersionInfo('swift3')
-
-#: Version string ``'major.minor.revision'``.
-version = _version_info.version_string()
+try:
+    # First, try to get our version out of PKG-INFO. If we're installed,
+    # this'll let us find our version without pulling in pbr. After all, if
+    # we're installed on a system, we're not in a Git-managed source tree, so
+    # pbr doesn't really buy us anything.
+    __version__ = pkg_resources.get_provider(
+        pkg_resources.Requirement.parse('swift3')).version
+except pkg_resources.DistributionNotFound:
+    # No PKG-INFO? We're probably running from a checkout, then. Let pbr do
+    # its thing to figure out a version number.
+    import pbr.version
+    __version__ = pbr.version.VersionInfo('swift3').release_string()
 
 #: Version information ``(major, minor, revision)``.
-version_info = version.split('.')
+version_info = tuple(map(int, __version__.split('.')[:3]))
+#: Version string ``'major.minor.revision'``.
+version = '.'.join(map(str, version_info))
