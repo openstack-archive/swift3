b21e7f0f3f6319f9d75ee9fd8dcebbd6c0e067fc
MORITA Kazutaka <morita.kazutaka@gmail.com>
2014-05-29 14:44:38 +0900

    Add a wrapper for lxml to handle XML namespace
    AWS S3 handles XML namespaces based on the following rules.
    
     - Any namespaces without a prefix work as the S3 namespace,
       http://s3.amazonaws.com/doc/2006-03-01/.
    
     - No namespace is same as having the S3 namespace.
    
    Examples of valid XMLs:
    
      * No namespace
        <AccessControlPolicy>
          <Owner><ID>id</ID></Owner>
          <AccessControlList/>
        </AccessControlPolicy>
    
      * No prefix namespace
        <AccessControlPolicy xmlns="http://example.com/">
          <Owner><ID>id</ID></Owner>
          <AccessControlList/>
        </AccessControlPolicy>
    
      * S3 namespace
        <s3:AccessControlPolicy xmlns:s3="http://s3.amazonaws.com/doc/2006-03-01/">
          <s3:Owner><s3:ID>id</s3:ID></s3:Owner>
          <s3:AccessControlList/>
        </s3:AccessControlPolicy>
    
      * Mixed namespace
        <AccessControlPolicy xmlns="http://example.com/"
           xmlns:s3="http://s3.amazonaws.com/doc/2006-03-01/">
          <Owner><s3:ID>id</s3:ID></Owner>
          <s3:AccessControlList/>
        </AccessControlPolicy>
    
    If the added namespace is not S3 but has a prefix, the xml is not valid.  For
    example, the following one is invalid for S3.
    
    <s3:AccessControlPolicy xmlns:s3="http://example.com/">
      <s3:Owner><s3:ID>id</s3:ID></s3:Owner>
      <s3:AccessControlList/>
    </s3:AccessControlPolicy>
    
    This patch adds support for the above namespace handling with a wrapper module
    for lxml.
    
    Change-Id: I4c15ce5bf3c7b4dd32bea4bba3e51bd12cce6478

---
+++ swift3/etree.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2014 OpenStack Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import lxml.etree
+from copy import deepcopy
+
+XMLNS_S3 = 'http://s3.amazonaws.com/doc/2006-03-01/'
+
+
+def cleanup_namespaces(elem):
+    def remove_ns(tag, ns):
+        if tag.startswith('{%s}' % ns):
+            tag = tag[len('{%s}' % ns):]
+        return tag
+
+    # remove s3 namespace
+    elem.tag = remove_ns(elem.tag, XMLNS_S3)
+
+    # remove default namespace
+    if elem.nsmap and None in elem.nsmap:
+        elem.tag = remove_ns(elem.tag, elem.nsmap[None])
+
+    for e in elem.iterchildren():
+        cleanup_namespaces(e)
+
+
+def fromstring(text):
+    elem = lxml.etree.fromstring(text)
+    cleanup_namespaces(elem)
+
+    return elem
+
+
+def tostring(tree, use_s3ns=True):
+    if use_s3ns:
+        nsmap = tree.nsmap.copy()
+        nsmap[None] = XMLNS_S3
+
+        root = lxml.etree.Element(tree.tag, attrib=tree.attrib, nsmap=nsmap)
+        root.text = tree.text
+        root.extend(deepcopy(tree.getchildren()))
+        tree = root
+
+    return lxml.etree.tostring(tree, xml_declaration=True, encoding='UTF-8')
+
+
+Element = lxml.etree.Element
+SubElement = lxml.etree.SubElement
