f9e7564a9bb1b1cac8b5319ec581439a5dc902d3
MORITA Kazutaka <morita.kazutaka@gmail.com>
2014-06-19 14:40:29 +0900

    test: split TestSwift3 based on controller modules
    This allows us to specify a controller to be tested.
    
    Change-Id: Ic14cd535c34574b081070455aeb1c3948294b379

---
+++ swift3/test/unit/test_acl.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2014 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from swift.common.swob import Request
+
+from swift3.test.unit import Swift3TestCase
+from swift3.etree import fromstring, tostring, Element, SubElement
+
+XMLNS_XSI = 'http://www.w3.org/2001/XMLSchema-instance'
+
+
+class TestSwift3Acl(Swift3TestCase):
+
+    def setUp(self):
+        super(TestSwift3Acl, self).setUp()
+
+    def _check_acl(self, owner, body):
+        elem = fromstring(body)
+        self.assertEquals(elem.tag, 'AccessControlPolicy')
+        permission = elem.find('./AccessControlList/Grant/Permission').text
+        self.assertEquals(permission, 'FULL_CONTROL')
+        name = elem.find('./AccessControlList/Grant/Grantee/ID').text
+        self.assertEquals(name, owner)
+
+    def test_bucket_acl_GET(self):
+        req = Request.blank('/bucket?acl',
+                            environ={'REQUEST_METHOD': 'GET'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        self._check_acl('test:tester', body)
+
+    def test_bucket_acl_PUT(self):
+        elem = Element('AccessControlPolicy')
+        owner = SubElement(elem, 'Owner')
+        SubElement(owner, 'ID').text = 'id'
+        acl = SubElement(elem, 'AccessControlList')
+        grant = SubElement(acl, 'Grant')
+        grantee = SubElement(grant, 'Grantee', nsmap={'xsi': XMLNS_XSI})
+        grantee.set('{%s}type' % XMLNS_XSI, 'Group')
+        SubElement(grantee, 'URI').text = \
+            'http://acs.amazonaws.com/groups/global/AllUsers'
+        SubElement(grant, 'Permission').text = 'READ'
+
+        xml = tostring(elem)
+        req = Request.blank('/bucket?acl',
+                            environ={'REQUEST_METHOD': 'PUT'},
+                            headers={'Authorization': 'AWS test:tester:hmac'},
+                            body=xml)
+        status, headers, body = self.call_swift3(req)
+        self.assertEquals(status.split()[0], '200')
+
+    def test_object_acl_GET(self):
+        req = Request.blank('/bucket/object?acl',
+                            environ={'REQUEST_METHOD': 'GET'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        self._check_acl('test:tester', body)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ swift3/test/unit/test_bucket.py
@@ -0,0 +1,194 @@
+# Copyright (c) 2014 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import cgi
+
+from swift.common import swob
+from swift.common.swob import Request
+
+from swift3.test.unit import Swift3TestCase
+from swift3.etree import fromstring
+
+
+class TestSwift3Bucket(Swift3TestCase):
+    def setup_objects(self):
+        self.objects = (('rose', '2011-01-05T02:19:14.275290', 0, 303),
+                        ('viola', '2011-01-05T02:19:14.275290', 0, 3909),
+                        ('lily', '2011-01-05T02:19:14.275290', 0, 3909),
+                        ('with space', '2011-01-05T02:19:14.275290', 0, 390),
+                        ('with%20space', '2011-01-05T02:19:14.275290', 0, 390))
+
+        json_pattern = ['"name":"%s"', '"last_modified":"%s"', '"hash":"%s"',
+                        '"bytes":%s']
+        json_pattern = '{' + ','.join(json_pattern) + '}'
+        json_out = []
+        for b in self.objects:
+            json_out.append(json_pattern % b)
+        object_list = '[' + ','.join(json_out) + ']'
+        self.swift.register('HEAD', '/v1/AUTH_test/junk', swob.HTTPNoContent,
+                            {}, None)
+        self.swift.register('GET', '/v1/AUTH_test/junk', swob.HTTPOk, {},
+                            object_list)
+
+    def setUp(self):
+        super(TestSwift3Bucket, self).setUp()
+
+        self.setup_objects()
+
+    def test_bucket_GET_error(self):
+        code = self._test_method_error('GET', '/bucket', swob.HTTPUnauthorized)
+        self.assertEquals(code, 'SignatureDoesNotMatch')
+        code = self._test_method_error('GET', '/bucket', swob.HTTPForbidden)
+        self.assertEquals(code, 'AccessDenied')
+        code = self._test_method_error('GET', '/bucket', swob.HTTPNotFound)
+        self.assertEquals(code, 'NoSuchBucket')
+        code = self._test_method_error('GET', '/bucket', swob.HTTPServerError)
+        self.assertEquals(code, 'InternalError')
+
+    def test_bucket_GET(self):
+        bucket_name = 'junk'
+        req = Request.blank('/%s' % bucket_name,
+                            environ={'REQUEST_METHOD': 'GET'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        self.assertEquals(status.split()[0], '200')
+
+        elem = fromstring(body)
+        self.assertEquals(elem.tag, 'ListBucketResult')
+        name = elem.find('./Name').text
+        self.assertEquals(name, bucket_name)
+
+        objects = elem.iterchildren('Contents')
+
+        names = []
+        for o in objects:
+            names.append(o.find('./Key').text)
+            self.assertTrue(o.find('./LastModified').text.endswith('Z'))
+
+        self.assertEquals(len(names), len(self.objects))
+        for i in self.objects:
+            self.assertTrue(i[0] in names)
+
+    def test_bucket_GET_is_truncated(self):
+        bucket_name = 'junk'
+
+        req = Request.blank('/%s' % bucket_name,
+                            environ={'REQUEST_METHOD': 'GET',
+                                     'QUERY_STRING': 'max-keys=5'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        elem = fromstring(body)
+        self.assertEquals(elem.find('./IsTruncated').text, 'false')
+
+        req = Request.blank('/%s' % bucket_name,
+                            environ={'REQUEST_METHOD': 'GET',
+                                     'QUERY_STRING': 'max-keys=4'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        elem = fromstring(body)
+        self.assertEquals(elem.find('./IsTruncated').text, 'true')
+
+    def test_bucket_GET_max_keys(self):
+        bucket_name = 'junk'
+
+        req = Request.blank('/%s' % bucket_name,
+                            environ={'REQUEST_METHOD': 'GET',
+                                     'QUERY_STRING': 'max-keys=5'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        elem = fromstring(body)
+        self.assertEquals(elem.find('./MaxKeys').text, '5')
+        _, path = self.swift.calls[-1]
+        _, query_string = path.split('?')
+        args = dict(cgi.parse_qsl(query_string))
+        self.assert_(args['limit'] == '6')
+
+        req = Request.blank('/%s' % bucket_name,
+                            environ={'REQUEST_METHOD': 'GET',
+                                     'QUERY_STRING': 'max-keys=5000'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        elem = fromstring(body)
+        self.assertEquals(elem.find('./MaxKeys').text, '1000')
+        _, path = self.swift.calls[-1]
+        _, query_string = path.split('?')
+        args = dict(cgi.parse_qsl(query_string))
+        self.assertEquals(args['limit'], '1001')
+
+    def test_bucket_GET_passthroughs(self):
+        bucket_name = 'junk'
+        req = Request.blank('/%s' % bucket_name,
+                            environ={'REQUEST_METHOD': 'GET', 'QUERY_STRING':
+                                     'delimiter=a&marker=b&prefix=c'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        elem = fromstring(body)
+        self.assertEquals(elem.find('./Prefix').text, 'c')
+        self.assertEquals(elem.find('./Marker').text, 'b')
+        self.assertEquals(elem.find('./Delimiter').text, 'a')
+        _, path = self.swift.calls[-1]
+        _, query_string = path.split('?')
+        args = dict(cgi.parse_qsl(query_string))
+        self.assertEquals(args['delimiter'], 'a')
+        self.assertEquals(args['marker'], 'b')
+        self.assertEquals(args['prefix'], 'c')
+
+    def test_bucket_PUT_error(self):
+        code = self._test_method_error('PUT', '/bucket', swob.HTTPCreated,
+                                       headers={'Content-Length': 'a'})
+        self.assertEqual(code, 'InvalidArgument')
+        code = self._test_method_error('PUT', '/bucket', swob.HTTPCreated,
+                                       headers={'Content-Length': '-1'})
+        self.assertEqual(code, 'InvalidArgument')
+        code = self._test_method_error('PUT', '/bucket', swob.HTTPUnauthorized)
+        self.assertEquals(code, 'SignatureDoesNotMatch')
+        code = self._test_method_error('PUT', '/bucket', swob.HTTPForbidden)
+        self.assertEquals(code, 'AccessDenied')
+        code = self._test_method_error('PUT', '/bucket', swob.HTTPAccepted)
+        self.assertEquals(code, 'BucketAlreadyExists')
+        code = self._test_method_error('PUT', '/bucket', swob.HTTPServerError)
+        self.assertEquals(code, 'InternalError')
+
+    def test_bucket_PUT(self):
+        req = Request.blank('/bucket',
+                            environ={'REQUEST_METHOD': 'PUT'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        self.assertEquals(status.split()[0], '200')
+
+    def test_bucket_DELETE_error(self):
+        code = self._test_method_error('DELETE', '/bucket',
+                                       swob.HTTPUnauthorized)
+        self.assertEquals(code, 'SignatureDoesNotMatch')
+        code = self._test_method_error('DELETE', '/bucket', swob.HTTPForbidden)
+        self.assertEquals(code, 'AccessDenied')
+        code = self._test_method_error('DELETE', '/bucket', swob.HTTPNotFound)
+        self.assertEquals(code, 'NoSuchBucket')
+        code = self._test_method_error('DELETE', '/bucket', swob.HTTPConflict)
+        self.assertEquals(code, 'BucketNotEmpty')
+        code = self._test_method_error('DELETE', '/bucket',
+                                       swob.HTTPServerError)
+        self.assertEquals(code, 'InternalError')
+
+    def test_bucket_DELETE(self):
+        req = Request.blank('/bucket',
+                            environ={'REQUEST_METHOD': 'DELETE'},
+                            headers={'Authorization': 'AWS test:tester:hmac'})
+        status, headers, body = self.call_swift3(req)
+        self.assertEquals(status.split()[0], '204')
+
+if __name__ == '__main__':
+    unittest.main()
