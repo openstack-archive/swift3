148a2984fd04e36f9eff68c62d7095f80775147b
MORITA Kazutaka <morita.kazutaka@gmail.com>
2014-06-25 02:43:35 +0900

    Added swift3.cfg
    We are unlikely to have multiple configuration instances in the swift3
    middleware.  Having one global variable for configuration makes swift3 codes
    simpler and much more configurable.
    
    This patch introduces swift3.cfg.  It is a tiny implementation to access config
    options with an oslo.config-like interface.  With this patch, we can access
    swift3 config anywhere via swift3.cfg.CONF.
    
    Change-Id: Ic620f9b673de0112770a46d56ed756d8991dee45

---
+++ swift3/cfg.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2014 OpenStack Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from swift.common.utils import config_true_value
+
+
+class Config(dict):
+    def __init__(self, base=None):
+        if base is not None:
+            self.update(base)
+
+    def __getattr__(self, name):
+        if name not in self:
+            raise AttributeError("No attribute '%s'" % name)
+
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        del self[name]
+
+    def update(self, other):
+        if hasattr(other, 'keys'):
+            for key in other.keys():
+                self[key] = other[key]
+        else:
+            for key, value in other:
+                self[key] = value
+
+    def __setitem__(self, key, value):
+        if isinstance(self.get(key), bool):
+            dict.__setitem__(self, key, config_true_value(value))
+        elif isinstance(self.get(key), int):
+            dict.__setitem__(self, key, int(value))
+        else:
+            dict.__setitem__(self, key, value)
+
+
+CONF = Config({
+    'location': 'US',
+    'max_bucket_listing': 1000,
+})
