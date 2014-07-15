8a66821fb5ab232f9ac6d5521a05d49e961c6afa
MORITA Kazutaka <morita.kazutaka@gmail.com>
2014-07-16 00:42:11 +0900

    service: ignores subresource queries for service requests
    S3 ignores subresource queries for service requests.  For example, the
    following request is handled as a GET Service API.
    
      GET /?acl
      Host: s3.amazonaws.com
    
    This patch addresses the behavior.
    
    Change-Id: I50f3e391048a3a69bc49980212693c1f85bd9d4f

--- swift3/request.py
+++ swift3/request.py
@@ -238,6 +238,9 @@ class Request(swob.Request):
 
     @property
     def controller(self):
+        if not self.container_name:
+            return ServiceController
+
         if 'acl' in self.params:
             return AclController
         if 'delete' in self.params:
@@ -262,10 +265,7 @@ class Request(swob.Request):
 
         if self.container_name and self.object_name:
             return ObjectController
-        elif self.container_name:
-            return BucketController
-
-        return ServiceController
+        return BucketController
 
     def to_swift_req(self, method, query=None):
         """
