c5c338c107882d2ddabf54026dfd150e4d342f30
MORITA Kazutaka <morita.kazutaka@gmail.com>
2014-07-28 03:36:23 +0900

    Make the swift3 logger global
    Unless we use multiple log_routes, making the logger global is simpler.
    
    Change-Id: I1fcce3b630371def7169a0e4b5f862f555c61e0f

--- swift3/controllers/base.py
+++ swift3/controllers/base.py
@@ -15,13 +15,8 @@
 
 import functools
 
-from swift.common.utils import get_logger
-
 from swift3.response import S3NotImplemented, InvalidRequest
-from swift3.utils import camel_to_snake
-from swift3.cfg import CONF
-
-LOGGER = get_logger(CONF, log_route='swift3')
+from swift3.utils import LOGGER, camel_to_snake
 
 
 def bucket_operation(func=None, err_resp=None, err_msg=None):
