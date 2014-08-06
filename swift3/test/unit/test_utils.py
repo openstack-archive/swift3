401387cbb1d697e94f594433cba233ef3b3e8305
MORITA Kazutaka <morita.kazutaka@gmail.com>
2014-08-06 23:32:37 +0900

    test/unit: allow running test directly
    Change-Id: Ib819ce7051f2152f8cca89ac28196209a56dd0c1

--- swift3/test/unit/test_utils.py
+++ swift3/test/unit/test_utils.py
@@ -32,3 +32,6 @@ class TestSwift3Utils(unittest.TestCase):
     def test_snake_to_camel(self):
         for s1, s2 in strs:
             self.assertEquals(s1, utils.snake_to_camel(s2))
+
+if __name__ == '__main__':
+    unittest.main()
