7d8ab390546fad31077acf70fd992a1e8cf344e3
MORITA Kazutaka <morita.kazutaka@gmail.com>
2014-08-13 05:00:02 +0900

    etree: add support for url-encoding
    Change-Id: I8e0395b6495e46605a77f38a583d62cf9ad62f84

--- swift3/controllers/bucket.py
+++ swift3/controllers/bucket.py
@@ -52,6 +52,11 @@ class BucketController(Controller):
         max_keys = int(req.params.get('max-keys', CONF.max_bucket_listing))
         max_keys = min(max_keys, CONF.max_bucket_listing)
 
+        encoding_type = req.params.get('encoding-type')
+        if encoding_type is not None and encoding_type != 'url':
+            err_msg = 'Invalid Encoding Method specified in Request'
+            raise InvalidArgument('encoding-type', encoding_type, err_msg)
+
         query = {
             'format': 'json',
             'limit': max_keys + 1,
@@ -73,6 +78,10 @@ class BucketController(Controller):
         SubElement(elem, 'Marker').text = req.params.get('marker')
         SubElement(elem, 'MaxKeys').text = str(max_keys)
         SubElement(elem, 'Delimiter').text = req.params.get('delimiter')
+
+        if encoding_type is not None:
+            SubElement(elem, 'EncodingType').text = encoding_type
+
         if max_keys > 0 and len(objects) == max_keys + 1:
             is_truncated = 'true'
         else:
@@ -95,7 +104,7 @@ class BucketController(Controller):
                 common_prefixes = SubElement(elem, 'CommonPrefixes')
                 SubElement(common_prefixes, 'Prefix').text = o['subdir']
 
-        body = tostring(elem)
+        body = tostring(elem, encoding_type=encoding_type)
 
         return HTTPOk(body=body, content_type='application/xml')
 
