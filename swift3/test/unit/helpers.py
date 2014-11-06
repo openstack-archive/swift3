b62e16f86f3868e66566aa09c8ed752e0e9d8f4d
Masaki Tsukuda <tsukuda.masaki@po.ntts.co.jp>
2014-11-06 19:35:55 +0900

    acl: add preliminary support for S3 ACL
    Currently, Swift3 sets and retrieves Swift ACLs for S3 ACL requests.  However,
    S3 ACL is too different from Swift ACL to implement the below reference.
      http://docs.aws.amazon.com/AmazonS3/latest/dev/S3_ACLs_UsingACLs.html
    With this patch, Swift3 uses its own metadata for ACL
    (e.g. X-Container-Sysmeta-Swift3-Acl) to achieve the best S3 compatibility.
    This patch only embeds the S3 ACL into the Swift metadata.  The swift3
    middleware does not use it for S3 requests yet; this will be addressed later.
    
    Change-Id: I4522910b6b3a0066f24caa98727fdeb85837e42b

--- swift3/test/unit/helpers.py
+++ swift3/test/unit/helpers.py
@@ -19,6 +19,7 @@ from copy import deepcopy
 from hashlib import md5
 from swift.common import swob
 from swift.common.utils import split_path
+from swift3.cfg import CONF
 
 
 class FakeSwift(object):
@@ -26,7 +27,7 @@ class FakeSwift(object):
     A good-enough fake Swift proxy server to use in testing middleware.
     """
 
-    def __init__(self):
+    def __init__(self, auth='tempauth'):
         self._calls = []
         self.req_method_paths = []
         self.swift_sources = []
@@ -34,7 +35,30 @@ class FakeSwift(object):
         # mapping of (method, path) --> (response class, headers, body)
         self._responses = {}
 
+    def _fake_auth_middleware(self, env):
+        if 'swift.authorize_override' in env:
+            return
+
+        if 'HTTP_AUTHORIZATION' not in env:
+            return
+
+        _, authorization = env['HTTP_AUTHORIZATION'].split(' ')
+        tenant_user, sign = authorization.rsplit(':', 1)
+        tenant, user = tenant_user.rsplit(':', 1)
+
+        path = env['PATH_INFO']
+        env['PATH_INFO'] = path.replace(tenant_user, 'AUTH_' + tenant)
+
+        env['REMOTE_USER'] = 'authorized'
+
+        # AccessDenied by default
+        env['swift.authorize'] = lambda req: swob.HTTPForbidden(request=req)
+
     def __call__(self, env, start_response):
+        if CONF.s3_acl:
+            self._fake_auth_middleware(env)
+
+        req = swob.Request(env)
         method = env['REQUEST_METHOD']
         path = env['PATH_INFO']
         _, acc, cont, obj = split_path(env['PATH_INFO'], 0, 4,
@@ -43,11 +67,11 @@ class FakeSwift(object):
             path += '?' + env['QUERY_STRING']
 
         if 'swift.authorize' in env:
-            resp = env['swift.authorize']()
+            resp = env['swift.authorize'](req)
             if resp:
                 return resp(env, start_response)
 
-        headers = swob.Request(env).headers
+        headers = req.headers
         self._calls.append((method, path, headers))
         self.swift_sources.append(env.get('swift.source'))
 
@@ -86,7 +110,6 @@ class FakeSwift(object):
                 self.uploaded[path][0]['Content-Type'] = env["CONTENT_TYPE"]
 
         # range requests ought to work, hence conditional_response=True
-        req = swob.Request(env)
         resp = resp_class(req=req, headers=headers, body=body,
                           conditional_response=True)
         return resp(env, start_response)
