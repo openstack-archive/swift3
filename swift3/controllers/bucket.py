36d0fed266fdf3a3cb69446a0b48fabebfa8e868
Kota Tsuyuzaki <tsuyuzaki.kota@lab.ntt.co.jp>
2014-12-12 01:15:02 -0800

    ACL Handlers
    To make controller classes clean, this patch introduces new
    mechanism called ACL Handlers. It is really useful for customizing
    acl checking algorithms for each controller.
    
    BaseAclHandler wraps basic Acl handling as same as current one.
    (i.e. it will check acl from ACL_MAP by using HEAD)
    
    In addition, we can make some extended custom classes with the same
    name of the controllers. (e.g. BucketAclHandler is for BucketController)
    They consist of method(s) for actual S3 method on controllers as follows.
    
    e.g.:
    class BucketAclHandler(BaseAclHandler):
       def PUT:
           << put acl handling algorithms here for PUT bucket >>
    
    Change-Id: I155cd6387c81c03a2092ecd933f4769e5148c591

--- swift3/controllers/bucket.py
+++ swift3/controllers/bucket.py
@@ -24,7 +24,6 @@ from swift3.etree import Element, SubElement, tostring, fromstring, \
 from swift3.response import HTTPOk, S3NotImplemented, InvalidArgument, \
     MalformedXML, InvalidLocationConstraint
 from swift3.cfg import CONF
-from swift3.subresource import ACL, Owner
 from swift3.utils import LOGGER
 
 MAX_PUT_BUCKET_BODY_SIZE = 10240
@@ -133,25 +132,10 @@ class BucketController(Controller):
                 # Swift3 cannot support multiple reagions now.
                 raise InvalidLocationConstraint()
 
-        if CONF.s3_acl:
-            req_acl = ACL.from_headers(req.headers,
-                                       Owner(req.user_id, req.user_id))
+        if 'HTTP_X_AMZ_ACL' in req.environ:
+            handle_acl_header(req)
 
-            # To avoid overwriting the existing bucket's ACL, we send PUT
-            # request first before setting the ACL to make sure that the target
-            # container does not exist.
-            resp = req.get_response(self.app)
-
-            # update metadata
-            req.bucket_acl = req_acl
-            # FIXME If this request is failed, there is a possibility that the
-            # bucket which has no ACL is left.
-            req.get_response(self.app, 'POST')
-        else:
-            if 'HTTP_X_AMZ_ACL' in req.environ:
-                handle_acl_header(req)
-
-            resp = req.get_response(self.app)
+        resp = req.get_response(self.app)
 
         resp.status = HTTP_OK
         resp.location = '/' + req.container_name
