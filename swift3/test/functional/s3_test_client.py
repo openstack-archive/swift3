dd1905d4f977a2bf89f9ea1cfbb3b3f98114c332
Kota Tsuyuzaki <tsuyuzaki.kota@lab.ntt.co.jp>
2015-03-09 16:09:05 -0700

    Add documentation on s3_test_client
    To keep our eyes clean, wrote down the definitions.
    
    Change-Id: I3a4823ecdcf2b0cf3096550adae8f4217ff82c2e

--- swift3/test/functional/s3_test_client.py
+++ swift3/test/functional/s3_test_client.py
@@ -23,12 +23,25 @@ RETRY_COUNT = 3
 
 class Connection(object):
     """
-    Manage Connection
+    Connection class used for S3 functional testing.
     """
     def __init__(self, aws_access_key=os.environ.get('ADMIN_ACCESS_KEY'),
                  aws_secret_key=os.environ.get('ADMIN_SECRET_KEY'),
                  user_id='%s:%s' % (os.environ.get('ADMIN_TENANT'),
-                 os.environ.get('ADMIN_USER'))):
+                                    os.environ.get('ADMIN_USER'))):
+        """
+        Initialize method.
+
+        :param aws_access_key: a string of aws access key
+        :param aws_secret_key: a string of aws secret key
+        :param user_id: a string consists of TENANT and USER name used for
+                        asserting Owner ID (not required S3Connection)
+
+        In default, Connection class will be initialized as admin user
+        behaves as:
+        user_test_admin = admin .admin
+
+        """
         self.aws_access_key = aws_access_key
         self.aws_secret_key = aws_secret_key
         self.user_id = user_id
@@ -41,6 +54,11 @@ class Connection(object):
                          calling_format=OrdinaryCallingFormat())
 
     def reset(self):
+        """
+        Reset all swift environment to keep clean. As a result by calling this
+        method, we can assume the backend swift keeps no containers and no
+        objects on this connection's account.
+        """
         exceptions = []
         for i in range(RETRY_COUNT):
             try:
@@ -65,6 +83,18 @@ class Connection(object):
 
     def make_request(self, method, bucket='', obj='', headers=None, body='',
                      query=None):
+        """
+        Wrapper method of S3Connection.make_request.
+
+        :param method: a string of HTTP request method
+        :param bucket: a string of bucket name
+        :param obj: a string of object name
+        :param headers: a dictionary of headers
+        :param body: a string of data binary sent to S3 as a request body
+        :param query: a string of HTTP query argument
+
+        :returns: a tuple of (int(status_code), headers dict, resposne body)
+        """
         response = \
             self.conn.make_request(method, bucket=bucket, key=obj,
                                    headers=headers, data=body,
@@ -76,7 +106,8 @@ class Connection(object):
 
 def get_tester_connection():
     """
-    Return tester connection
+    Return tester connection behaves as:
+    user_test_tester = testing .admin
     """
     aws_access_key = os.environ.get('TESTER_ACCESS_KEY')
     aws_secret_key = os.environ.get('TESTER_SECRET_KEY')
@@ -87,7 +118,8 @@ def get_tester_connection():
 
 def get_tester2_connection():
     """
-    Return tester2 connection
+    Return tester2 connection behaves as:
+    user_test_tester2 = testing2 .admin
     """
     aws_access_key = os.environ.get('TESTER2_ACCESS_KEY')
     aws_secret_key = os.environ.get('TESTER2_SECRET_KEY')
