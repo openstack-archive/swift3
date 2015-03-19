275006bc4db6efca8dff07d16f0435080b17fd15
Naoto Nishizono <nishizono.naoto@po.ntts.co.jp>
2015-03-19 19:37:25 +0900

    Add functional tests for normal statuses of Multipart Upload
    This patch adds functional tests for normal statuses of Multipart Upload.
    
    Add following API's normal tests.
    - Initiate Multipart Upload
    - List Multipart Uploads
    - Upload Part
    - Upload Part Copy
    - List Parts
    - Abort Multipart Upload
    - Complete Multipart Upload
    
    blueprint functional-tests
    
    Change-Id: I1e55702f52fcf8dd7be4e1170daf44330dd3e4d6

--- swift3/test/functional/s3_test_client.py
+++ swift3/test/functional/s3_test_client.py
@@ -67,11 +67,15 @@ class Connection(object):
 
                 for bucket in buckets:
                     try:
+                        for upload in bucket.list_multipart_uploads():
+                            upload.cancel_upload()
+
                         for obj in bucket.list():
                             bucket.delete_key(obj.name)
+
                         self.conn.delete_bucket(bucket.name)
                     except S3ResponseError as e:
-                        # 404 means NoSuchBucket or NoSuchKey
+                        # 404 means NoSuchBucket, NoSuchKey, or NoSuchUpload
                         if e.status != 404:
                             raise
             except (BotoClientError, S3ResponseError) as e:
