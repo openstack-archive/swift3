ddb5ec86aac8f4ce7a92c2f0c31a33011eba7455
Tim Burke <tim.burke@gmail.com>
2015-06-19 17:56:00 -0700

    Only return InvalidDigest if Content-MD5 is not an MD5
    Previously, we would also use it on MD5 mismatch, but that should
    instead receive BadDigest.
    
    Change-Id: I990bb0d723f97325ab9cf0f5a6c920103125fbc2

--- swift3/test/unit/test_middleware.py
+++ swift3/test/unit/test_middleware.py
@@ -242,6 +242,22 @@ class TestSwift3Middleware(Swift3TestCase):
         status, headers, body = self.call_swift3(req)
         self.assertEquals(self._get_error_code(body), 'InvalidDigest')
 
+    def test_object_create_bad_md5_too_short(self):
+        req = Request.blank('/bucket/object',
+                            environ={'REQUEST_METHOD': 'PUT',
+                                     'HTTP_AUTHORIZATION': 'AWS X:Y:Z',
+                                     'HTTP_CONTENT_MD5': 'xxxx'})
+        status, headers, body = self.call_swift3(req)
+        self.assertEquals(self._get_error_code(body), 'InvalidDigest')
+
+    def test_object_create_bad_md5_too_long(self):
+        req = Request.blank('/bucket/object',
+                            environ={'REQUEST_METHOD': 'PUT',
+                                     'HTTP_AUTHORIZATION': 'AWS X:Y:Z',
+                                     'HTTP_CONTENT_MD5': '1B2M2Y8AsgTpgAmY7PhCfgo='})
+        status, headers, body = self.call_swift3(req)
+        self.assertEquals(self._get_error_code(body), 'InvalidDigest')
+
     def test_invalid_metadata_directive(self):
         req = Request.blank('/',
                             environ={'REQUEST_METHOD': 'GET',
