2fac5c13875e51a268acd9d6f8353506ca288ea6
Ondřej Nový <ondrej.novy@firma.seznam.cz>
2016-02-22 22:39:18 +0100

    Lower expire timestamp in test.
    If this number is higher than signed 32b int, tests are failing on 32
    bit systems. This will brake tests of Swift3 in 2038, sorry for it :).
    
    Change-Id: I5c76aa80aa3f16f61d5faa0925609c53766fd7a2

--- swift3/test/unit/test_middleware.py
+++ swift3/test/unit/test_middleware.py
@@ -166,7 +166,9 @@ class TestSwift3Middleware(Swift3TestCase):
         self.assertEquals(self._get_error_code(body), 'AccessDenied')
 
     def test_signed_urls(self):
-        expire = '10000000000'
+        # Set expire to last 32b timestamp value
+        # This number can't be higher, because it breaks tests on 32b systems
+        expire = '2147483647' # 19 Jan 2038 03:14:07
         req = Request.blank('/bucket/object?Signature=X&Expires=%s&'
                             'AWSAccessKeyId=test:tester' % expire,
                             environ={'REQUEST_METHOD': 'GET'},
