273165e473dc23b92d5bc1c9b9794b3b3bca9fa7
Morgan Fainberg <morgan.fainberg@gmail.com>
2016-04-28 14:23:23 -0500

    Add s3_token middleware to the swift3 project
    This patch moves (as discussed at the Newton design summit) the
    s3_token middleware from keystonemiddleware to swift3. The git
    history is not included based upon the agreement between the
    Keystone team and the Swift3 team.
    
    Change-Id: I1c0e68a5276dd3dee97d7569e477c784db8ccb8a

--- swift3/utils.py
+++ swift3/utils.py
@@ -17,6 +17,7 @@ import re
 import uuid
 import base64
 import time
+import types
 
 
 from swift.common.utils import get_logger
@@ -31,6 +32,9 @@ from swift3.cfg import CONF
 LOGGER = get_logger(CONF, log_route='swift3')
 
 MULTIUPLOAD_SUFFIX = '+segments'
+
+TRUE_STRINGS = ('1', 't', 'true', 'on', 'y', 'yes')
+FALSE_STRINGS = ('0', 'f', 'false', 'off', 'n', 'no')
 
 
 def sysmeta_prefix(resource):
@@ -136,6 +140,40 @@ def validate_bucket_name(name):
             return False
         else:
             return True
+
+
+def bool_from_string(subject, strict=False, default=False):
+    """Interpret a string as a boolean.
+
+    A case-insensitive match is performed such that strings matching 't',
+    'true', 'on', 'y', 'yes', or '1' are considered True and, when
+    `strict=False`, anything else returns the value specified by 'default'.
+
+    Useful for JSON-decoded stuff and config file parsing.
+
+    If `strict=True`, unrecognized values, including None, will raise a
+    ValueError which is useful when parsing values passed in from an API call.
+    Strings yielding False are 'f', 'false', 'off', 'n', 'no', or '0'.
+    """
+    if isinstance(subject, bool):
+        return subject
+    if not isinstance(subject, types.StringTypes):
+        subject = types.UnicodeType(subject)
+
+    lowered = subject.strip().lower()
+
+    if lowered in TRUE_STRINGS:
+        return True
+    elif lowered in FALSE_STRINGS:
+        return False
+    elif strict:
+        acceptable = ', '.join(
+            "'%s'" % s for s in sorted(TRUE_STRINGS + FALSE_STRINGS))
+        msg = ("Unrecognized value '%(val)s', acceptable values are: "
+               "%(acceptable)s") % {'val': subject, 'acceptable': acceptable}
+        raise ValueError(msg)
+    else:
+        return default
 
 
 class S3Timestamp(utils.Timestamp):
