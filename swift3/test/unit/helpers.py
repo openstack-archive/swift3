037dd0754493589624dee25e84134ead0c1f72f8
Tim Burke <tim.burke@gmail.com>
2016-06-17 10:25:14 -0700

    Don't try to read a body if the client didn't send one
    From RFC 7230 [1]:
    
    > The presence of a message body in a request is signaled by a
    > Content-Length or Transfer-Encoding header field.
    
    Thus, if a client sent neither a Content-Length nor a Transfer-Encoding
    header, we should assume there is no body and never read from
    wsgi.input. In such a case, return an empty string unless the caller
    has specified that a body is required.
    
    [1] https://tools.ietf.org/html/rfc7230#section-3.3
    
    Change-Id: I47ad946cff64a3da16a759a4364e6fe29b000e11
    Closes-Bug: 1593870

--- swift3/test/unit/helpers.py
+++ swift3/test/unit/helpers.py
@@ -150,3 +150,26 @@ class FakeSwift(object):
                     headers.update({key: value})
 
         self._responses[(method, path)] = (response_class, headers, body)
+
+
+class UnreadableInput(object):
+    # Some clients will send neither a Content-Length nor a Transfer-Encoding
+    # header, which will cause (some versions of?) eventlet to bomb out on
+    # reads. This class helps us simulate that behavior.
+    def __init__(self, test_case):
+        self.calls = 0
+        self.test_case = test_case
+
+    def read(self, *a, **kw):
+        self.calls += 1
+        # Calling wsgi.input.read with neither a Content-Length nor
+        # a Transfer-Encoding header will raise TypeError (See
+        # https://bugs.launchpad.net/swift3/+bug/1593870 in detail)
+        # This unreadable class emulates the behavior
+        raise TypeError
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.test_case.assertEqual(0, self.calls)
