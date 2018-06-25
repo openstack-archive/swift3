32a24ea54f3aba52ca8f5fa3c77714a164b13121
Tim Burke <tim.burke@gmail.com>
2018-06-25 12:50:16 -0700

    s3_acl: Require swift_owner authz to create buckets
    Otherwise, users can create buckets in accounts they don't own.
    
    Change-Id: I7decd9c1b4af0d03b6b9a29a9245f6c4536153cb

--- swift3/acl_handlers.py
+++ swift3/acl_handlers.py
@@ -17,7 +17,7 @@ import sys
 
 from swift3.subresource import ACL, Owner, encode_acl
 from swift3.response import MissingSecurityHeader, \
-    MalformedACLError, UnexpectedContent
+    MalformedACLError, UnexpectedContent, AccessDenied
 from swift3.etree import fromstring, XMLSyntaxError, DocumentInvalid
 from swift3.utils import LOGGER, MULTIUPLOAD_SUFFIX, sysmeta_header
 
@@ -181,6 +181,9 @@ class BucketAclHandler(BaseAclHandler):
     def PUT(self, app):
         req_acl = ACL.from_headers(self.req.headers,
                                    Owner(self.user_id, self.user_id))
+
+        if not self.req.environ.get('swift_owner'):
+            raise AccessDenied()
 
         # To avoid overwriting the existing bucket's ACL, we send PUT
         # request first before setting the ACL to make sure that the target
